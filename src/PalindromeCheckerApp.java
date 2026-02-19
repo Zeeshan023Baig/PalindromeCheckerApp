4658e7d402301889dcc3d74e9829856903408a60
O Zeeshan Baig <zeeshan.baig.1323@gmail.com>
2026-02-19 10:30:50 +0530

    Character array palindrome check

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,18 +1,19 @@
 public class PalindromeCheckerApp{
     public static void main(String[] args){
-        String text= "lokol";
-        String reversed = "";
-        System.out.println("Input text: " + text);
-        boolean palindrome =true;
-        for (int i= text.length()-1; i>=0;i--){
-            reversed+=text.charAt(i);
+        String input = "radar";
+        char[]chars = input.toCharArray();
+        int start = 0;
+        int end = chars.length -1;
+        boolean isPalindrome = true;
+        while (start<end){
+            if (chars[start] != chars[end]){
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
-        if (text.equals(reversed)){
-            System.out.println("Is it a palindrome? : True");
-        }
-        else{
-            System.out.println("Is it a palindrome? : False");
-
-        }
+        System.out.println("Input:"+input);
+        System.out.println("Is it palindrome? :"+isPalindrome);
     }
 }
